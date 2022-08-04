4e24e5b937f4778cee38ef839c8358ef41be9d37
bean <email1six3@163.com>
2022-08-04 21:05:40 +0800

    递归思维训练1.0

---
+++ Python/pythonProject_4liu/file4digui.py
@@ -0,0 +1,38 @@
+# 递归训练：1-100累加的和
+# 非递归思想：
+def sum1(start, end):
+    acc = 0
+    for n in range(start, end + 1):
+        acc += n
+    return acc
+
+
+result = sum1(1, 100)
+print("Non-Recursive:", result)
+
+del sum1
+
+
+# 递归思想，利用递归函数：
+def sum1(start, end):
+    if start < end:
+        acc = start + sum1(start + 1, end)
+        return acc
+    else:
+        return end
+
+
+result = sum1(1, 100)
+print("Recursive:", result)
+
+del sum1
+
+
+# 高斯算法
+def sum1(start, end):
+
+    return(start+end)*(end-start+1)/2
+
+
+result = sum1(1, 100)
+print("Gauss:", result)
